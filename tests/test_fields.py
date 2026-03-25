73ab44bb54314ad4e5832584bd62ee8c24ca5dc1
Abby Austin <38941820+spidertyler2005@users.noreply.github.com>
2026-03-24 20:15:38 -0400

    fix validation of lists + more tests

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -53,11 +53,88 @@ def test_int_field_auto_create():
         test = comprehensiveconfig.spec.Integer(10)
 
     assert Foo.test == 10
-    Foo.test = 11  # manually set instance value
+    Foo.test = 11
     assert Foo.test == 11
 
     try:
         Foo.test = 12.12
-        assert Foo.test == 11
+        assert False
     except ValueError:
         assert Foo.test == 11
+
+
+def test_float_field_auto_create():
+    class Foo(
+        comprehensiveconfig.ConfigSpec,
+        auto_load=True,
+        writer=comprehensiveconfig.toml.TomlWriter,
+        default_file=OUTPUT_DIR + "/test.toml",
+        create_file=True,
+    ):
+        """Basic config"""
+
+        test = comprehensiveconfig.spec.Float(10.12)
+
+    assert Foo.test == 10.12
+    Foo.test = 11
+    assert Foo.test == 11
+
+    try:
+        Foo.test = "chumpus"
+        assert False
+    except ValueError:
+        assert Foo.test == 11
+
+
+def test_text_field_auto_create():
+    class Foo(
+        comprehensiveconfig.ConfigSpec,
+        auto_load=True,
+        writer=comprehensiveconfig.toml.TomlWriter,
+        default_file=OUTPUT_DIR + "/test.toml",
+        create_file=True,
+    ):
+        """Basic config"""
+
+        test = comprehensiveconfig.spec.Text("clean")
+
+    assert Foo.test == "clean"
+    Foo.test = "bean"
+    assert Foo.test == "bean"
+
+    try:
+        Foo.test = 12
+        assert False
+    except ValueError:
+        assert Foo.test == "bean"
+
+
+def test_list_field_auto_create():
+    class Foo(
+        comprehensiveconfig.ConfigSpec,
+        auto_load=True,
+        writer=comprehensiveconfig.toml.TomlWriter,
+        default_file=OUTPUT_DIR + "/test.toml",
+        create_file=True,
+    ):
+        """Basic config"""
+
+        test = comprehensiveconfig.spec.List(
+            ["12", "13", "14"], inner_type=comprehensiveconfig.spec.Text()
+        )
+
+    assert Foo.test == ["12", "13", "14"]
+    Foo.test = ["fee", "fi", "foh"]
+    assert Foo.test == ["fee", "fi", "foh"]
+
+    try:
+        Foo.test = ["fee", 0, "foh"]
+        assert False
+    except ValueError:
+        assert Foo.test == ["fee", "fi", "foh"]
+
+    try:
+        Foo.test = "hi"
+        assert False
+    except ValueError:
+        assert Foo.test == ["fee", "fi", "foh"]
